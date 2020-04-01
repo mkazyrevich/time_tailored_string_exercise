0b738e73f3ad9ebe677b531a7510a34fbe0457ad
Maksim Kazyrevich <mkozyrevich@gmail.com>
2020-04-02 00:57:34 +0300

    init: Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function getSpentTime(string) {
+  return {
+    i: 205,
+    v: 147,
+    e: 90
+  }
+}
+
+export {getSpentTime}
---
+++ test/test.js
@@ -0,0 +1,25 @@
+import {getSpentTime} from '../index'
+
+let assert = require('chai').assert;
+
+describe('getSpentTime()', function() {
+  var tests = [
+    {string: 'all I did today; i 20m, 35m, 2.5h, 2h40m v 40m 35m 1.2h e 30, 60m', expected: {
+      i: 205,
+      v: 147,
+      e: 90
+    }},
+    {string: 'all I did today; i  30m, 35m, 2m, 5m, 3h30m v 40m 35m 1.5h sef 350, 60', expected: {
+      i: 282,
+      v: 165,
+      sef: 410
+    }}
+  ];
+
+  tests.forEach(function(test) {
+    it('test', function() {
+      let res = JSON.stringify(getSpentTime(test.string));
+      assert.equal(res, JSON.stringify(test.expected));
+    });
+  });
+});
